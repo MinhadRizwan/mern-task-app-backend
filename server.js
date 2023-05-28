b00ff23fc4abbb661673fba9e64dc3002d68ecc3
MinhadRizwan <minhadmohammedrizwan@gmail.com>
2023-05-29 01:17:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import Todo from "./models/Todo.js";
 const app = express();
 const secret = 'secret123';
 // const PORT = process.env.BASE_URL;
-
+BASE_URL=process.env.BASE_URL;
 await mongoose.connect('mongodb+srv://minhad:secret123@minhadcluster.f6lviky.mongodb.net/Task-Manager?retryWrites=true&w=majority', {useNewUrlParser: true})
 // mongoose.connect(process.env.MONGO_URI)
 .then(()=>{

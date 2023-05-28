45b79cc92e4b356af2ed7d681c666f58cf4b5b0a
MinhadRizwan <minhadmohammedrizwan@gmail.com>
2023-05-28 23:06:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const secret = 'secret123';
 const PORT = process.env.PORT;
 
-await mongoose.connect('mongodb+srv://minhad:secret123@minhadcluster.f6lviky.mongodb.net/Task-Manager?retryWrites=true&w=majority', {useNewUrlParser: true})
+await mongoose.connect('mongodb+srv://minhad:secret123@minhadcluster.f6lviky.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true})
 // mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
   app.listen(PORT,()=>{

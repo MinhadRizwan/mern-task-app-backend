817513d1cbd019002873e10cc04e37946f969201
MinhadRizwan <minhadmohammedrizwan@gmail.com>
2023-05-28 15:22:50 +0530

    commit

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ import jwt from 'jsonwebtoken';
 import Todo from "./models/Todo.js";
 
 const secret = 'secret123';
-
-await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true});
-const db = mongoose.connection;
-db.on('error', console.log);
+const PORT = process.env.PORT || 4000;
+await mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+  app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+  })
+})
+// const db = mongoose.connection;
+// db.on('error', console.log);
 
 const app = express();
 app.use(cookieParser());
